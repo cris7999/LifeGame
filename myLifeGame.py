0675bdb0d9b0b487a85cef3c851bb2a2b28ea990
cris7999 <cris7999@hotmail.com>
2020-04-20 13:19:34 +0200

    Fix update, logic working

--- myLifeGame.py
+++ myLifeGame.py
@@ -4,53 +4,121 @@ def random_grid(n):
     grid = np.array([])
     numeric_grid = np.random.rand(n,n).round()
     grid=np.zeros((n, n), dtype=bool)
-    print(numeric_grid)
+
     for i in range(len(numeric_grid)):
         for j in range(len(numeric_grid)):
             if (numeric_grid[i][j] == 1):
                 grid[i][j] = True
             else:
                 grid[i][j] = False
-    print (grid)
     return grid
 
-def show_grid():
+def show_grid(grid):
     print(grid)
 
 def count_neighbors(i , j):
     count = 0
+    real_len=len(grid) - 1
+    visited = False
+    #corners
     if(i == 0 and j==0):
-        if(grid[i + 1][j] is True):
+        visited = True
+        if(grid[i + 1][j]):
             count += 1
-        if(grid[i][j + 1] is True):
+        if(grid[i][j + 1]):
             count +=1
-        if(grid[i +1][j + 1] is True):
+        if(grid[i +1][j + 1]):
             count +=1
 
-    elif (i == 0 and j == len(grid)):
-        if(grid[i + 1][j] is True):
+    elif (i == 0 and j == real_len and (not visited)):
+        visited = True
+        if(grid[i + 1][j]):
             count += 1
-        if(grid[i][j - 1] is True):
+        if(grid[i][j - 1]):
             count +=1
-        if(grid[i - 1][j - 1] is True):
+        if(grid[i - 1][j - 1]):
             count +=1
 
-    elif (i == len(grid) and j == 0):
-        if(grid[i][j + 1] is True):
+    elif (i == real_len and j == 0 and (not visited)):
+        visited = True
+        if(grid[i][j + 1]):
             count += 1
-        if(grid[i - 1][j] is True):
+        if(grid[i - 1][j]):
             count +=1
-        if(grid[i - 1][j + 1] is True):
+        if(grid[i - 1][j + 1]):
             count +=1
 
-    elif (i == len(grid) and j == len(grid)):
-        if(grid[i][j - 1] is True):
+    elif (i == real_len and j == real_len and (not visited)):
+        visited = True
+        if(grid[i][j - 1]):
             count += 1
-        if(grid[i - 1][j] is True):
+        if(grid[i - 1][j]):
             count +=1
-        if(grid[i - 1][j - 1] is True):
+        if(grid[i - 1][j - 1]):
             count +=1
-        
+    #edges
+    elif (i == 0 and (not visited)):
+        visited = True
+        if(grid[i + 1][j]):
+            count += 1
+        if(grid[i][j + 1]):
+            count +=1
+        if(grid[i + 1][j + 1]):
+            count +=1
+        if(grid[i + 1][j - 1]):
+            count +=1
+        if(grid[i][j - 1]):
+            count +=1
+    
+    elif (i == real_len and (not visited)):
+        visited = True
+        if(grid[i - 1][j]):
+            count += 1
+        if(grid[i][j + 1]):
+            count +=1
+        if(grid[i - 1][j + 1]):
+            count +=1
+        if(grid[i - 1][j - 1]):
+            count +=1
+        if(grid[i][j - 1]):
+            count +=1
+    
+    elif (j == 0 and (not visited)):
+        visited = True
+        if(grid[i][j + 1]):
+                count += 1
+        if(grid[i + 1][j]):
+            count +=1
+        if(grid[i + 1][j + 1]):
+            count +=1
+        if(grid[i - 1][j + 1]):
+            count +=1
+        if(grid[i - 1][j]):
+            count +=1
+
+    elif( j == real_len and (not visited)):
+        visited = True
+        if(grid[i][j - 1]):
+            count += 1
+        if(grid[i + 1][j]):
+            count +=1
+        if(grid[i + 1][j - 1]):
+            count +=1
+        if(grid[i - 1][j - 1]):
+            count +=1
+        if(grid[i - 1][j]):
+            count +=1
+    if (not visited):
+        visited = True
+        if(grid[i][j - 1]):
+            count += 1
+        if(grid[i][j + 1]):
+            count +=1
+        if(grid[i - 1][j]):
+            count +=1
+        if(grid[i + 1][j]):
+            count +=1
+
     return count
 
 
@@ -58,7 +126,7 @@ def updateGrid():
     for i in range(len(grid)):
         for j in range(len(grid)):
             num_neighbors = count_neighbors(i , j)
-            if(grid[i][j] is True):
+            if(grid[i][j]):
                 if((num_neighbors < 2) or (num_neighbors > 3)):
                     grid[i][j] = False
             else:
@@ -68,8 +136,8 @@ def updateGrid():
     return grid        
                     
 
-grid = random_grid(3)
-show_grid()
-updateGrid()
+grid = random_grid(5)
+show_grid(grid)
+grid = updateGrid()
 print("ACTUALIZACION:")
-show_grid()
+show_grid(grid)
